456ccdac74de605da1c85e5efb947c091ca99944
Timilehin <olajolotaiwo2@gmail.com>
2024-10-13 18:38:16 +0100

    RandomWord.java

--- RandomWord.java
+++ RandomWord.java
@@ -1,22 +1,19 @@
-
-
 import edu.princeton.cs.algs4.StdIn;
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stdout;
 import edu.princeton.cs.algs4.StdRandom;
 
 public class RandomWord {
     public static void main(String[] args) {
-        String champ = "";
+        String champion = "";
         int i = 1;
         while (!StdIn.isEmpty()) {
-            String value = StdIn.readString();
-            boolean flag = StdRandom.bernoulli((double) 1 / i);
+            String word = StdIn.readString();
+            boolean flag = StdRandom.bernoulli(1.0 / i);
             if (flag) {
-                champ = value;
+                champion = word;
             }
             i++;
         }
-            StdOut.println(champ);
-
+        Stdout.println(champion);
     }
 }
